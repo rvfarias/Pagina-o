fe46f68385468d9f9451cff395f95087bd936e00
rvfarias <rvldf2000@gmail.com>
2023-05-29 12:53:01 -0300

    add FIFO and OTM functions

--- main.cpp
+++ main.cpp
@@ -6,18 +6,83 @@
 
 using namespace std;
 
+//Implementação do FIFO
 void pag_FIFO(const vector<int> &data_arq, int numQuadros){
 
+    //Declaracao de variaveis
     vector<int> paginas, ram;
     vector<int>::iterator it;
     
     int fail = 0;
     bool flag;
 
+    //Copiando dados do arquivo para poder manipular
     for (int i = 0; i < data_arq.size(); i++){
         paginas.push_back(data_arq[i]);
     }
     
+    //Loop de execucao
+    while (paginas.size()){
+
+        // cout <<"Restante: " << paginas.size() << endl;
+        flag = false;
+        
+        //Verificando se a página já está na ram
+        for (int i = 0; i < ram.size(); i++){
+            //Se estiver seta a flag pra true
+            if (paginas[0] == ram[i]){
+                flag = true;
+                break;
+            }
+        }
+        
+        //Caso a página não esteja na ram
+        if (!flag){
+            //E ainda tiver espaço livre 
+            if (ram.size() < numQuadros){
+                //Adiciona a pagina na ram e apaga a mesma do array de paginas 
+                ram.push_back(paginas[0]);
+                paginas.erase(paginas.begin());
+
+            }else{
+                //Se não tiver espaço tira o primeiro elemento da ram e adiciona o novo elemento no final do array
+                ram.erase(ram.begin());
+                ram.push_back(paginas[0]);
+                paginas.erase(paginas.begin());
+            }
+
+            //Incrementa o numero de falhas 
+            fail++;
+
+        }else{
+            //Se a página estiver na ram,apenas apagamos ela do array de páginas 
+            paginas.erase(paginas.begin());
+        }
+        
+    }
+
+    //Printa o numero de falhas
+    cout << "FIFO "<< fail << endl;
+
+}
+
+//Implementação do OTM
+void pag_OTM(const vector<int> &data_arq, int numQuadros){
+
+    //Declaracao de variaveis
+    vector<int> paginas, ram;
+    vector<int>::iterator it;
+
+    int aux1, aux2;
+    int fail = 0;
+    bool flag;
+
+    //Copiando dados do arquivo para poder manipular
+    for (int i = 0; i < data_arq.size(); i++){
+        paginas.push_back(data_arq[i]);
+    }
+
+    //Loop de execucao
     while (paginas.size()){
 
         // cout <<"Restante: " << paginas.size() << endl;
@@ -32,6 +97,7 @@ void pag_FIFO(const vector<int> &data_arq, int numQuadros){
         }
         
         if (!flag){
+
             if (ram.size() < numQuadros){
 
                 ram.push_back(paginas[0]);
@@ -39,7 +105,47 @@ void pag_FIFO(const vector<int> &data_arq, int numQuadros){
 
             }else{
 
-                ram.erase(ram.begin());
+                if (paginas.size() <= ram.size()){
+
+                    for (int i = 0; i < ram.size(); i++){     
+                        flag = false;
+                        for (int j = 0; j < paginas.size(); j++){
+                            if (ram[i] == paginas[j]){
+                                flag =true;
+                            }
+                            
+                        }
+
+                        if (!flag){
+                            aux2 = i;
+                            break;
+                        }
+                        
+                    }
+                
+                }else{
+
+                    aux1 = -1;
+                    for (int i = 0; i < ram.size(); i++){                
+                        for (int j = 0; j < paginas.size(); j++){
+                            if (ram[i] == paginas[j] && aux1 < j){
+                                // cout << "old: " << aux2 << endl; 
+
+                                aux1 = j;
+                                aux2 = i;
+                                // cout << "new: " << aux2 << endl;
+                                break;
+                            }
+                            
+                        }
+                    }
+                    // cout << "troca: " << ram[aux2] << " por " << paginas[0] << endl; 
+                }
+                
+                
+
+                
+                ram.erase(ram.begin()+ aux2);
                 ram.push_back(paginas[0]);
                 paginas.erase(paginas.begin());
             }
@@ -59,13 +165,12 @@ void pag_FIFO(const vector<int> &data_arq, int numQuadros){
 
     }
 
-    cout << "FIFO "<< fail << endl;
+    cout << "OTM "<< fail << endl;
+
+    
 }
 
-void pag_OTM(){
-
-}
-
+//Implementação do LRU
 void pag_LRU(){
 
 }
@@ -93,7 +198,7 @@ int main(int argc, char const *argv[]){
 
     arq.close();
 
-
+    pag_FIFO(paginas, numQuadros);
 
     return 0;
 }
